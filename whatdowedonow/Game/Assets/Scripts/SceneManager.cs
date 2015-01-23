0d74a6bd50c5d536c9cf6e6c9f143cfe179dc790
Joey <joey___boy@hotmail.com>
2015-01-23 23:41:51 +0100

    maked the randomSceneNumber static so we can use it in all the classes.

--- whatdowedonow/Game/Assets/Scripts/SceneManager.cs
+++ whatdowedonow/Game/Assets/Scripts/SceneManager.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class SceneManager : MonoBehaviour {
 
-	public int randomSceneNumber;
+	public static int randomSceneNumber;
 
 	// Use this for initialization
 	void Start () {
