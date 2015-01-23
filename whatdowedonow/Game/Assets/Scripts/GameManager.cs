612e53923a78c694d01ca3c2ac80b4af0e9a1679
Joey <joey___boy@hotmail.com>
2015-01-23 21:06:16 +0100

    Removed code which is not used. Created a prefab for the player

--- whatdowedonow/Game/Assets/Scripts/GameManager.cs
+++ whatdowedonow/Game/Assets/Scripts/GameManager.cs
@@ -3,11 +3,6 @@ using System.Collections;
 
 public class GameManager : MonoBehaviour {
 
-	public GUIText goalText;
-	public GUIText timerText;
-	public GUIText endStateText;
-	public GUIText winStateText;
-	
 	public static int numberOfKeysLeft = 10;
 	public static float globalTime = 30;
 
@@ -19,25 +14,6 @@ public class GameManager : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-		// Set the goal text equals to how much keys are left
-		goalText.text = numberOfKeysLeft.ToString();
 
-		// Count down the globalTime
-		if (globalTime > 0 && numberOfKeysLeft > 0) {
-			globalTime -= (Time.deltaTime * 0.25f);		
-		}
-
-		// Checks if the timer is 0
-		if (globalTime <= 0 && numberOfKeysLeft != 0) {
-				Player1.GameOver ();
-				endStateText.text = "Game Over!!";
-				//endStateText.enabled = true;
-
-			} else if (numberOfKeysLeft == 0 && globalTime > 0) {
-				Player1.GameWon();
-				winStateText.text = "Level complete!";
-				}
-
-		timerText.text = "Time: " + globalTime.ToString("f0");
 	}
 }
