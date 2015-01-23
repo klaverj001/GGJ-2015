ce2a4b2530ce0a30929fe94a6bb3203fd70b00a1
jimmy <klaverj001@gmail.com>
2015-01-23 20:31:47 +0100

    basic project

---
+++ whatdowedonow/Game/Assets/Scripts/GameManager.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	public GUIText goalText;
+	public GUIText timerText;
+	public GUIText endStateText;
+	public GUIText winStateText;
+	
+	public static int numberOfKeysLeft = 10;
+	public static float globalTime = 30;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		// Set the goal text equals to how much keys are left
+		goalText.text = numberOfKeysLeft.ToString();
+
+		// Count down the globalTime
+		if (globalTime > 0 && numberOfKeysLeft > 0) {
+			globalTime -= (Time.deltaTime * 0.25f);		
+		}
+
+		// Checks if the timer is 0
+		if (globalTime <= 0 && numberOfKeysLeft != 0) {
+				Player1.GameOver ();
+				endStateText.text = "Game Over!!";
+				//endStateText.enabled = true;
+
+			} else if (numberOfKeysLeft == 0 && globalTime > 0) {
+				Player1.GameWon();
+				winStateText.text = "Level complete!";
+				}
+
+		timerText.text = "Time: " + globalTime.ToString("f0");
+	}
+}
