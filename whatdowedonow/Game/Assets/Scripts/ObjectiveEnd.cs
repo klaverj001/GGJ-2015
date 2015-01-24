5376509b30a0209c765e2ec8569a52416fcd860b
k-biesterveld <kevin@mindgame.nl>
2015-01-24 20:12:22 +0100

    Add functionality that calls the first objective

---
+++ whatdowedonow/Game/Assets/Scripts/ObjectiveEnd.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectiveEnd : MonoBehaviour {
+
+	//The gameobject that contains the ObjectManager script.
+	public GameObject game;
+	
+	void OnTriggerEnter2D (Collider2D other)
+	{
+		if (other.gameObject.GetComponent ("Player1")) 
+		{
+			game.GetComponent<ObjectManager>().Objective1();
+		}
+	}
+}
