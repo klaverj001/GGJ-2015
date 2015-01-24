e6631a57b55a3adfd9c610932aeace610365dafa
k-biesterveld <kevin@mindgame.nl>
2015-01-24 23:14:37 +0100

    Add objective 3

---
+++ whatdowedonow/Game/Assets/Scripts/Objective3.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Objective3 : MonoBehaviour {
+
+	//The gameobject that contains the ObjectManager script.
+	public GameObject game;
+	private float timer = 0f;
+	public float timerSeconds = 5.0f;
+	private bool timerActive = false;
+
+
+	void Update () 
+	{
+		if (timerActive == true) 
+		{
+			timer += Time.deltaTime;
+
+			if(timer >= timerSeconds)
+			{
+				timerActive = false;
+				game.GetComponent<ObjectManager>().Objective(3);
+			}
+		}
+	}
+
+
+	void OnCollisionEnter2D (Collision2D other)
+	{
+		if (other.gameObject.tag == "Player")
+		{
+			timerActive = true;
+		}
+	}
+
+	void OnCollisionExit2D (Collision2D other)
+	{
+		if (other.gameObject.tag == "Player")
+		{
+			timerActive = false;
+			timer = 0f;
+		}
+	}
+}
