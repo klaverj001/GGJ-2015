acf5a488da703d734229f735d27b698bafd90f31
k-biesterveld <kevin@mindgame.nl>
2015-01-24 15:22:15 +0100

    Add prefab crusher obstacle + scripts

---
+++ whatdowedonow/Game/Assets/Scripts/CrusherEmpty.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CrusherEmpty : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	//OncollisionEnter
+	void OnCollisionEnter2D (Collision2D other) 
+	{
+		if (other.gameObject.GetComponent("Player1")) 
+		{
+			Destroy(other.gameObject);
+		}
+	}
+}
